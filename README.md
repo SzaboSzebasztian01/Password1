dc34a088c7be05172edfdbc675ad75c63bcc487d
Szabó_Szebasztián <szaboszebasztianlevente@ktch.hu>
2023-02-27 15:37:52 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Password
+
+Jelszó tároló, konzolos változat. s
+
+2023-02-27
+
+El Dondé újra lecsap
---
+++ src/MainMenu.java
@@ -0,0 +1,15 @@
+public class MainMenu {
+
+    public MainMenu() {
+    }
+    
+    public void showMenu(){
+        System.out.println();
+        System.out.println("----------------------");
+        System.out.println("1., Megtekintés");
+        System.out.println("1., Új jelszó");
+        System.out.println("----------------------");
+        System.out.println();
+    }
+
+}
